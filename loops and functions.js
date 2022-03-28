c512f9222dc7d384380f0877678b5ec7a8380778
MarianS23 <49622608+MarianS23@users.noreply.github.com>
2022-03-28 12:22:26 +0300

    Create loops and functions.js

---
+++ loops and functions.js
@@ -0,0 +1,32 @@
+//task1 
+let nArrey = [2,3,4,5];
+let product = 1;
+
+for(let i = 0;i<nArrey.length;i++){
+    product *= nArrey[i];
+};
+console.log(product);
+
+
+
+let nArrey = [2,3,4,5];
+let product = 1;
+let i = 0;
+
+while(i<nArrey.length){
+	product *= nArrey[i];
+	i++;
+}
+console.log(product);
+
+
+
+//tasl 2 
+for(let i = 0;i<=15;i++){
+	if(i % 2==0){
+		console.log(i +" is odd");
+	}else{
+		console.log(i +" is even");
+	};
+	
+};
