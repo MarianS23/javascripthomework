566f0de1c88346b1b8bf25c2de8599abe0b91b81
MarianS23 <49622608+MarianS23@users.noreply.github.com>
2022-04-01 14:54:52 +0300

    Update loops and functions.js

--- loops and functions.js
+++ loops and functions.js
@@ -30,3 +30,69 @@ for(let i = 0;i<=15;i++){
 	};
 	
 };
+
+
+
+//task 3
+function randArray(k){
+	let myArray = [];
+	for(let i= 0;i<k;i++){
+		myArray[i]=Math.floor(Math.random()*500)+1;
+	};
+	return myArray;
+};
+
+
+
+//task 4 
+function raiseToDegree(a,b){
+	let result = a;
+	for(let i = 1;i<b;i++){
+		result *= a;
+	};
+	return result;
+};
+
+let a = +prompt("enter number");
+let b = +prompt("enter degree");
+if(Number.isInteger(a) && Number.isInteger(b)){
+	alert(raiseToDegree(a,b));
+}else{
+        alert("you enter incorrect data");
+	};
+        
+//task 5
+function findMin(){
+	let minNum = arguments[0];
+	for(let i = 1;i<arguments.length;i++){
+		if(arguments[i] <minNum){
+			minNum= arguments[i];
+		};
+	};return minNum;
+};
+
+//task 6
+function findUniq(arr){
+	for(let i = 0;i<arr.length;i++){
+		for(let j = (i + 1);j<arr.length;j++){
+			if(arr[i]===arr[j]){
+				return false;
+			}
+		}
+	}
+};
+
+//task 7
+function showLastElem(arr,lastElem){
+	return (lastElem == null)?arr.pop():arr.slice(-lastElem);
+};
+
+//task 9
+function changeFirsLetter (str){
+	let array = str.slice(" ");
+	let newArray = [];
+	 for(let i = 0;array.length;i++){
+		newArray.push();
+	 };
+	return newArray.join(" ")
+};
