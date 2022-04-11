93156fc6796bede8e0cf1b1ff12140e343db1c19
MarianS23 <49622608+MarianS23@users.noreply.github.com>
2022-04-11 12:34:38 +0300

    Create OOP.js

---
+++ OOP.js
@@ -0,0 +1,39 @@
+//task 1
+function propsCount(currentObject){
+	let myArray = Object.values(currentObject);
+	return myArray.length;
+}
+
+
+//task 2
+function CreatesObject(first,second,third,fourth,fifth){
+         this.first = first;
+         this.second = second;
+         this.third = third;
+         this.fourth = fourth;
+         this.fifth = fifth;
+};
+
+
+let myObj = new CreatesObject(1,2,3,4,5);
+
+
+function showProps(obj){
+	let myArray = Object.values(obj);
+	return myArray;
+};
+
+
+
+//task 3      ще доробляю!!
+
+class Person{
+	constructor(name,surname){
+		this.name = name;
+		this.surname = surname;	
+	}
+	showFullName(){
+		//return this.name + " "+ this.surname  не правильно
+		return `${this.name} ${this.surname}`
+	}
+}
